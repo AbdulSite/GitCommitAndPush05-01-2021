273ba23f56c3c01878a26531b6aa74412350908f
araha <araha@LAPTOP-CLOFSUPE>
2021-01-03 16:31:31 +0530

    Add 16:31

--- Scripts/CreatedOrganizationPresentOrNotInTableList/Script1604582372381.groovy
+++ Scripts/CreatedOrganizationPresentOrNotInTableList/Script1604582372381.groovy
@@ -44,7 +44,7 @@ By obj = By.xpath("//table/tbody/tr/td[1]")
 By objTwo = By.xpath("//*[@title='Next Page']")
 
 CustomKeywords.'customKeywords.commanFunctions.openBrowser'()
-
+//
 CustomKeywords.'customKeywords.commanFunctions.logIntoBrowser'()
 
 //CustomKeywords.'customKeywords.commanFunctions.otpLogin'()
