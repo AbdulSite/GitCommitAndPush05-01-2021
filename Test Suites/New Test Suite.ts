8ab2fea0f7702bc3f0b51473eef714f4cb8eb073
araha <araha@LAPTOP-CLOFSUPE>
2021-01-03 16:15:29 +0530

    Iniial Detect Project Commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1b2abb79-93d4-49fa-8127-390d8b9a0172</testSuiteGuid>
+   <testCaseLink>
+      <guid>09daada7-5736-4a7d-9514-67f4caec00bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestObjectInitializationInTC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
